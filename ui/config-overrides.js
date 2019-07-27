eeac8001387979a22c8b2adb4117f048759f6c16
Max Ferguson <maxkferg@gmail.com>
2019-07-27 04:03:35 -0700

    Changed the homepage

--- ui/config-overrides.js
+++
@@ -1,8 +0,0 @@
-/* config-overrides.js */
-
-const rewireInlineImportGraphqlAst = require('react-app-rewire-inline-import-graphql-ast');
-
-module.exports = function override(config, env) {
-  config = rewireInlineImportGraphqlAst(config, env);
-  return config;
-}
--- ui/src/apollo/index.tsx
+++ ui/src/apollo/index.tsx
@@ -5,5 +5,6 @@ let port = "8888";
 
 export default new ApolloClient({
 	//uri: "http://localhost:8888/graphql"
-	uri: host + ":" + port + "/graphql"
+	//uri: host + ":" + port + "/graphql"
+	uri: "http://api.digitalpoints.io/graphql"
 });

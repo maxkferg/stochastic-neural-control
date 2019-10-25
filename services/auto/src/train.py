3c9f3c1e024229ad01c874f3186b85290eb660de
Max Ferguson <maxkferg@gmail.com>
2019-10-25 00:46:47 -0700

    Performant GPU code

--- services/auto/src/train.py
+++ services/auto/src/train.py
@@ -12,8 +12,9 @@ python train.py --cuda_idx=0
 """
 from rlpyt.envs.gym import make as gym_make
 from rlpyt.utils.launching.affinity import encode_affinity
-from rlpyt.utils.launching.affinity import affinity_from_code
-from rlpyt.samplers.parallel.cpu.sampler import CpuSampler
+from rlpyt.utils.launching.affinity import prepend_run_slot, affinity_from_code
+#from rlpyt.samplers.parallel.cpu.sampler import CpuSampler
+from rlpyt.samplers.async_.gpu_sampler import AsyncGpuSampler
 from rlpyt.algos.qpg.sac import SAC
 from rlpyt.agents.qpg.sac_agent import SacAgent
 from rlpyt.runners.minibatch_rl import MinibatchRlEval
@@ -29,7 +30,21 @@ register(
     entry_point='environment.env.simple:SimpleEnvironment',
 )
 
+
 def build_and_train(env_id="Seeker-v0", run_ID=0, cuda_idx=None):
+    affinity_code = encode_affinity(
+        n_cpu_core=4,
+        n_gpu=1,
+        # contexts_per_gpu=2,
+        async_sample=True,
+        # hyperthread_offset=2,
+        # n_socket=1,
+        # cpu_per_run=1,
+    )
+    slot_affinity_code = prepend_run_slot(0, affinity_code)
+    affinity = affinity_from_code(slot_affinity_code)
+    print("Affinity:", affinity)
+
     env_config = dict(
         headless=True
     )
@@ -38,31 +53,39 @@ def build_and_train(env_id="Seeker-v0", run_ID=0, cuda_idx=None):
         headless=True
     )
  
-    sampler = CpuSampler(
+    sampler = AsyncGpuSampler(
         EnvCls=gym_make,
         env_kwargs=dict(id=env_id, config=env_config),
         eval_env_kwargs=dict(id=env_id, config=eval_env_config),
         batch_T=1,  # One time-step per sampler iteration.
-        batch_B=4,  # One environment (i.e. sampler Batch dimension).
+        batch_B=1,  # One environment (i.e. sampler Batch dimension).
         max_decorrelation_steps=0,
         eval_n_envs=4,
         eval_max_steps=int(10e3),
         eval_max_trajectories=10,
     )
 
-    algo = SAC()
+    algo = SAC(
+        reward_scale=0.5,
+        learning_rate=3e-4,
+        reparameterize=True,
+        target_entropy="auto",
+    )
+
     agent = SacAgent(
         ModelCls=PiMlpModel,
         QModelCls=QofMuMlpModel,
     )
-    runner = MinibatchRlEval(
+
+    runner = AsyncRlEval(
         algo=algo,
         agent=agent,
         sampler=sampler,
         n_steps=1e5,
-        affinity=dict(cuda_idx=cuda_idx, workers_cpus=[1,2,3,4]),
+        affinity=affinity,
         log_interval_steps=5e3
     )
+
     config = dict(env_id=env_id)
     name = "sac_" + env_id
     log_dir = "example_1"

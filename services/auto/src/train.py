ee89162cbb89448773b3dc3fb16b533a98377bc9
Max Ferguson <maxkferg@gmail.com>
2019-10-25 01:34:37 -0700

    Good GPU config

--- services/auto/src/train.py
+++ services/auto/src/train.py
@@ -14,7 +14,8 @@ from rlpyt.envs.gym import make as gym_make
 from rlpyt.utils.launching.affinity import encode_affinity
 from rlpyt.utils.launching.affinity import prepend_run_slot, affinity_from_code
 #from rlpyt.samplers.parallel.cpu.sampler import CpuSampler
-from rlpyt.samplers.async_.gpu_sampler import AsyncGpuSampler
+from rlpyt.samplers.async_.cpu_sampler import AsyncCpuSampler
+#from rlpyt.samplers.async_.gpu_sampler import AsyncGpuSampler
 from rlpyt.algos.qpg.sac import SAC
 from rlpyt.agents.qpg.sac_agent import SacAgent
 from rlpyt.runners.minibatch_rl import MinibatchRlEval
@@ -33,13 +34,11 @@ register(
 
 def build_and_train(env_id="Seeker-v0", run_ID=0, cuda_idx=None):
     affinity_code = encode_affinity(
-        n_cpu_core=4,
+        n_cpu_core=8,
         n_gpu=1,
-        # contexts_per_gpu=2,
+        #contexts_per_gpu=2,
         async_sample=True,
-        # hyperthread_offset=2,
-        # n_socket=1,
-        # cpu_per_run=1,
+        #hyperthread_offset=2,
     )
     slot_affinity_code = prepend_run_slot(0, affinity_code)
     affinity = affinity_from_code(slot_affinity_code)
@@ -53,12 +52,12 @@ def build_and_train(env_id="Seeker-v0", run_ID=0, cuda_idx=None):
         headless=True
     )
  
-    sampler = AsyncGpuSampler(
+    sampler = AsyncCpuSampler(
         EnvCls=gym_make,
         env_kwargs=dict(id=env_id, config=env_config),
         eval_env_kwargs=dict(id=env_id, config=eval_env_config),
         batch_T=1,  # One time-step per sampler iteration.
-        batch_B=1,  # One environment (i.e. sampler Batch dimension).
+        batch_B=6,  # One environment (i.e. sampler Batch dimension).
         max_decorrelation_steps=0,
         eval_n_envs=4,
         eval_max_steps=int(10e3),
@@ -81,9 +80,9 @@ def build_and_train(env_id="Seeker-v0", run_ID=0, cuda_idx=None):
         algo=algo,
         agent=agent,
         sampler=sampler,
-        n_steps=1e5,
+        n_steps=1e7,
         affinity=affinity,
-        log_interval_steps=5e3
+        log_interval_steps=10e3
     )
 
     config = dict(env_id=env_id)

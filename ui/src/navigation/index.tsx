d8afccb5896c480b4befff17ea8195a986aab37e
Duy Tran <dqtran@axon.com>
2020-01-01 11:33:49 +0700

    refactor routers componentg

--- ui/src/navigation/index.tsx
+++ ui/src/navigation/index.tsx
@@ -1,53 +1,52 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch } from 'react-router';
 import AuthenticationRoute from './AuthenticationRoute';
 import App from '../containers/App/App';
 import About from '../containers/About';
 import Error from '../pages/error/Error';
 import Auth from '../pages/login/Login'
-import Landing from '../containers/Landing';
 import DashBoard from '../pages/dashboard/Dashboard';
+import Landing from '../containers/Landing';
 import { verifyToken } from '../services/AuthServices';
 import { LayoutProvider } from "../context/LayoutContext";
 import { connect } from 'react-redux';
 import { setCurrentUser } from '../redux/actions/currentUser';
-class Routers extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	async componentDidMount() {
-		try {
-			const verifyTokenResp = await verifyToken();
-			//@ts-ignore
-			if (verifyTokenResp.data.verifyToken.id) {
-				//@ts-ignore
-				this.props.setCurrentUser(verifyTokenResp.data.verifyToken);
-				//@ts-ignore
-				// customHistory.push('/buildings');
-			}
-		} catch {
-			localStorage.removeItem('token');
-		}
-	}
-	render() {
-		return (
-			<LayoutProvider>
-				<Switch>
-				<Route exact path="/auth" component={Auth} />
-				<Route exact path="/about" component={About} />
-				<Route exact path="/" component={Landing} /> 
-				<AuthenticationRoute exact path="/app/buildings" component={DashBoard} /> 
-				<AuthenticationRoute path="/app/building/:buildingId" component={App}/>
-				<Route component={Error} />
-				</Switch>
-			</LayoutProvider>
-		)
-	}
+
+
+interface RoutersProps {
+	setCurrentUser: Function
 }
 
+const Routers: React.FC<RoutersProps> = props => {
+	const { setCurrentUser } = props;
+	useEffect(() => {
+		async function verifyTokenToken() {
+			try {
+				const verifyTokenResp = await verifyToken();
+				if (verifyTokenResp.data.verifyToken.id) {
+					setCurrentUser(verifyTokenResp.data.verifyToken);
+				}
+			} catch {
+				localStorage.removeItem('token');
+			}
+		}
+		verifyTokenToken()
+	}, [])
+	return (
+		<LayoutProvider>
+			<Switch>
+			<Route exact path="/auth" component={Auth} />
+			<Route exact path="/about" component={About} />
+			<Route exact path="/" component={Landing} /> 
+			<AuthenticationRoute exact path="/app/buildings" component={DashBoard} /> 
+			<AuthenticationRoute path="/app/building/:buildingId" component={App}/>
+			<Route component={Error} />
+			</Switch>
+		</LayoutProvider>
+	)
+}
 
-
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = (dispatch) : RoutersProps=> ({
 	setCurrentUser: (payload) => dispatch(setCurrentUser(payload))
 })
 export default connect(null, mapDispatchToProps)(Routers)
